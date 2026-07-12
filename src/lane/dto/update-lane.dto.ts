88213d0ca6ebe39257408e775a33156efaed721d
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-07-12 18:39:52 -0300

    fix update pipes validation for parent ids

--- src/lane/dto/update-lane.dto.ts
+++ src/lane/dto/update-lane.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLaneDto } from './create-lane.dto';
+import { IsOptional, IsUUID } from 'class-validator';
 
-export class UpdateLaneDto extends PartialType(CreateLaneDto) {}
+export class UpdateLaneDto extends PartialType(CreateLaneDto) {
+  @IsOptional()
+  @IsUUID()
+  board?: string;
+}
--- src/tags/dto/update-tag.dto.ts
+++ src/tags/dto/update-tag.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTagDto } from './create-tag.dto';
+import { IsOptional, IsUUID } from 'class-validator';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {}
+export class UpdateTagDto extends PartialType(CreateTagDto) {
+  @IsOptional()
+  @IsUUID()
+  task?: string;
+}
