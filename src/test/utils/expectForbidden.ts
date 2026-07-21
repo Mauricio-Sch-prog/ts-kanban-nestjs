9963da0ddd73f0098e6837443e3eedb8ecd29bb1
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-07-21 20:56:26 -0300

    add expect borbidden util

---
+++ src/test/utils/expectForbidden.ts
@@ -0,0 +1,38 @@
+import { INestApplication } from '@nestjs/common';
+import { Response } from 'supertest';
+import { loginRequest } from './loginRequest';
+import { getCookies } from './getCookies';
+
+interface baseProduct {
+  id: string;
+}
+
+type createRequestFunc = (
+  app: INestApplication,
+  cookies: string[],
+) => Promise<baseProduct>;
+
+type forbiddenRequestFunc = (
+  id: string,
+  badCookies: string[],
+) => Promise<Response>;
+
+export const expectForbidden = async (
+  app: INestApplication,
+  createRequest: createRequestFunc,
+  forbiddenRequest: forbiddenRequestFunc,
+) => {
+  const user1 = await loginRequest(app, { email: 'a@test.com' });
+  const cookies1 = getCookies(user1);
+
+  const user2 = await loginRequest(app, { email: 'b@test.com' });
+  const cookies2 = getCookies(user2);
+
+  const product = await createRequest(app, cookies1);
+
+  const id = product.id;
+
+  const res = await forbiddenRequest(id, cookies2);
+
+  expect(res.status).toEqual(403);
+};
