55522b7562db2f193bad82c61b370e613279268c
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-09-07 13:42:43 -0300

    update UpdateTaskDto

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  title?: string;
+
   @IsOptional()
   @IsUUID()
   lane?: string;
